9fadbd7e48b5cf5866e08fbab69ed756c878254c
Pavel Pawlowski <pavel.pawlowski@hotmail.cz>
2017-11-21 14:27:15 +0100

    Parameters and Procedure name update
    * Update to parameter names to conform with sp_ScheduleStatus
    * Name Case update to conform with sp_ScheduleStatus

--- msdb/sp_jobstatus.sql
+++ msdb/sp_jobstatus.sql
@@ -1,29 +1,29 @@
 USE [msdb]
-IF NOT EXISTS(SELECT * FROM sys.procedures WHERE object_id = OBJECT_ID('[dbo].[sp_jobstatus]'))
-    EXEC (N'CREATE PROCEDURE [dbo].[sp_jobstatus] AS PRINT ''Placeholder for [dbo].[sp_jobstatus]''')
+IF NOT EXISTS(SELECT * FROM sys.procedures WHERE object_id = OBJECT_ID('[dbo].[sp_JobStatus]'))
+    EXEC (N'CREATE PROCEDURE [dbo].[sp_JobStatus] AS PRINT ''Placeholder for [dbo].[sp_JobStatus]''')
 GO
 /* ****************************************************
-sp_jobstatus v 0.20 (2017-03-02)
+sp_JobStatus v 0.20 (2017-03-02)
 (C) 2017 Pavel Pawlowski
 
 Feedback: mailto:pavel.pawlowski@hotmail.cz
 
 License: 
-    sp_jobstatus is free to download and use for personal, educational, and internal 
+    sp_JobStatus is free to download and use for personal, educational, and internal 
     corporate purposes, provided that this header is preserved. Redistribution or sale 
-    of sp_jobstatus, in whole or in part, is prohibited without the author's express 
+    of sp_JobStatus, in whole or in part, is prohibited without the author's express 
     written consent.
 
 Description:
     Provides information about processes in SSISDB
 
 Parameters:
-    @filter                 nvarchar(max)   = NULL      --Comma separated list of LIKE filter to limit jobs. When not provided all jobs are printed
-    ,@job_status_to_script  bit             = 1         --Status of the jobs to be printed. 1 = Enabled, 0 - Disabled, NULL = both disable and enabled
+    @filter     nvarchar(max)   = NULL      --Comma separated list of LIKE filter to limit jobs. When not provided all jobs are printed
+    ,@status    bit             = 1         --Status of the jobs to be printed. 1 = Enabled, 0 - Disabled, NULL = both disable and enabled
  ******************************************************* */
-ALTER PROCEDURE [dbo].[sp_jobstatus]
-    @filter                 nvarchar(max)   = NULL      --Comma separated list of LIKE filter to limit jobs. When not provided all jobs are printed
-    ,@job_status_to_script  bit             = 1         --Status of the jobs to be printed. 1 = Enabled, 0 - Disabled, NULL = both disable and enabled
+ALTER PROCEDURE [dbo].[sp_JobStatus]
+    @filter     nvarchar(max)   = NULL      --Comma separated list of LIKE filter to limit jobs. When not provided all jobs are printed
+    ,@status    bit             = 1         --Status of the jobs to be printed. 1 = Enabled, 0 - Disabled, NULL = both disable and enabled
 AS
 SET NOCOUNT ON;
 DECLARE
@@ -33,20 +33,20 @@ DECLARE
     ,@job_id_str    nvarchar(50)
     ,@xml           xml
 
-RAISERROR(N'--sp_jobstatus v0.20 (2017-03-02) (c) 2017 Pavel Pawlowski', 0, 0) WITH NOWAIT;
-RAISERROR(N'--==========================================================', 0, 0) WITH NOWAIT;
-RAISERROR(N'--sp_jobstatus enables or disables jobs', 0, 0) WITH NOWAIT;
+RAISERROR(N'--sp_JobStatus v0.21 (2017-11-21) (c) 2017 Pavel Pawlowski', 0, 0) WITH NOWAIT;
+RAISERROR(N'--========================================================', 0, 0) WITH NOWAIT;
+RAISERROR(N'--sp_JobStatus enables or disables jobs', 0, 0) WITH NOWAIT;
 RAISERROR(N'', 0, 0) WITH NOWAIT;
 
 IF @filter = N'?'
 BEGIN
     RAISERROR(N'
-Usage: sp_jobstatus [parameters]
+Usage: sp_JobStatus [parameters]
 
 Params:
-    @filter                 nvarchar(max)   = NULL      --Comma separated list of LIKE filter to limit jobs. When not provided all jobs are printed.
-                                                          filter prefixed by [-] removes jobs from selection
-    ,@job_status_to_script  bit             = 1         --Status of the jobs to be printed. 1 = Enabled, 0 - Disabled, NULL = both disable and enabled
+    @filter     nvarchar(max)   = NULL  --Comma separated list of LIKE filter to limit jobs. When not provided all jobs are printed.
+                                          filter prefixed by [-] removes jobs from selection
+    ,@status    bit             = 1     --Status of the jobs to be printed. 1 = Enabled, 0 - Disabled, NULL = both disable and enabled
 
 ', 0, 0) WITH NOWAIT;
     RETURN;
@@ -61,7 +61,7 @@ SET @filter = ISNULL(NULLIF(@filter, N''), N'%')
 SET @xml = N'<i>' + REPLACE(@filter, N',', N'</i><i>') + N'</i>'
 
 
-SET @msg ='DECLARE @enabled bit = ' + CASE WHEN @job_status_to_script = 0 THEN N'0' ELSE N'1' END + N'    --Specify status to set 1 = Enabled, 0 = Disabled'
+SET @msg ='DECLARE @enabled bit = ' + CASE WHEN @status = 0 THEN N'0' ELSE N'1' END + N'    --Specify status to set 1 = Enabled, 0 = Disabled'
 RAISERROR(@msg, 0, 0) WITH NOWAIT;
 
 RAISERROR(N'', 0,0) WITH NOWAIT;
@@ -75,7 +75,7 @@ SELECT DISTINCT
 FROM msdb.dbo.sysjobs j
 INNER JOIN (SELECT LTRIM(RTRIM(n.value('.', 'nvarchar(128)'))) FROM @xml.nodes('/i') T(n)) F(n) ON j.name LIKE F.n
 WHERE 
-    enabled = @job_status_to_script OR @job_status_to_script IS NULL
+    enabled = @status OR @status IS NULL
     AND
     LEFT(F.n, 1) <> '-'
 EXCEPT
@@ -85,15 +85,12 @@ SELECT
 FROM msdb.dbo.sysjobs j
 INNER JOIN (SELECT LTRIM(RTRIM(n.value('.', 'nvarchar(128)'))) FROM @xml.nodes('/i') T(n)) F(n) ON j.name LIKE RIGHT(F.n, LEN(F.n) - 1) AND LEFT(F.n, 1) = '-'
 WHERE 
-    enabled = @job_status_to_script OR @job_status_to_script IS NULL
+    enabled = @status OR @status IS NULL
     AND
     LEFT(F.n, 1) = '-'
 
 
 OPEN cr;
-
-
---RAISERROR('DECLARE @enabled bit = 
 
 RAISERROR(N'', 0,0) WITH NOWAIT;
 
